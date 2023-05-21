5aa28f1525f653ba9635cbf0ed72ec20c828bb1d
AuracleTech <virgilfabre@outlook.com>
2023-05-21 13:05:32 -0700

    Improved recorder which trims on silence threshold

--- recorder.py
+++ recorder.py
@@ -1,29 +1,72 @@
-import sounddevice as sd
-import wavio as wv
-import datetime
 import os
+import datetime
+import pyaudio
+import wave
+import librosa
+import numpy as np
 
 def listen(stop_event, recordings_path):
-    # Get the sample frequency of the sound device
-    FREQ = sd.query_devices(None, 'input')['default_samplerate']
-    # Set the duration of the recording
-    DURATION = 5 # seconds
+    # Matching whisper settings
+    FORMAT = pyaudio.paInt16
+    # OpenAI whisper uses 1 channel by default
+    CHANNELS = 1
+    # OpenAI whisper uses 16kHz sample rate by default
+    RATE = 16000
     # Set the sample duration
-    CHUNK = int(FREQ * DURATION)
+    CHUNK = 160
+
+    SILENCE_RMS_CEILING = 64
+    SILENCE_MIN_FRAMES = CHUNK / 4
+
+    # Initialize PyAudio
+    audio = pyaudio.PyAudio()
+
+    # Open microphone stream
+    stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
 
     print('Listening...')
     while not stop_event.is_set():
-        ts = datetime.datetime.now()
-        filename = ts.strftime("%Y-%m-%d_%H-%M-%S")
+        frames = []
+        silent_frames = 0
 
-        # Start recorder with the given values of duration and sample frequency
-        # PTL Note: I had to change the channels value in the original code to fix a bug
-        recording = sd.rec(CHUNK, samplerate=FREQ, channels=2)
+        while True:
+            # Read audio data from the stream
+            data = stream.read(CHUNK)
+            frames.append(data)
 
-        # Record audio for the given number of seconds
-        sd.wait()
+            # Check for silence
+            chunk = np.frombuffer(data, dtype=np.int16)
+            rms_features = librosa.feature.rms(y=chunk, frame_length=CHUNK, hop_length=CHUNK)
+            rms = np.average(rms_features)
 
-        # Convert the NumPy array to audio file
-        wv.write(os.path.join(recordings_path, f"{filename}.wav"), recording, FREQ, sampwidth=2)
+            # If the audio is silent, increment the silent frame count
+            if rms < SILENCE_RMS_CEILING:
+                silent_frames += 1
+            else:
+                silent_frames = 0
+            
+            # If there are consecutive silent frames, stop recording
+            if silent_frames >= SILENCE_MIN_FRAMES:
+                break
+        
+        # If all frames are silent, skip saving the file
+        if len(frames) == silent_frames:
+            continue
+
+        # Save the recorded audio to a WAV file
+        ts = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        filename = f"{ts}.wav"
+        file_path = os.path.join(recordings_path, filename)
+
+        with wave.open(file_path, 'wb') as wf:
+            wf.setnchannels(CHANNELS)
+            wf.setsampwidth(audio.get_sample_size(FORMAT))
+            wf.setframerate(RATE)
+            wf.writeframes(b"".join(frames))
+
+    # Close the stream
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
 
     print('Stopped listening')
