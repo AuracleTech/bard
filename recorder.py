2321e87dc2dae247e6fa55c8cc1c1a492eb3834e
AuracleTech <virgilfabre@outlook.com>
2023-05-21 10:37:42 -0700

    Set constants

--- recorder.py
+++ recorder.py
@@ -5,10 +5,11 @@ import os
 
 def listen(stop_event, recordings_path):
     # Get the sample frequency of the sound device
-    freq = sd.query_devices(None, 'input')['default_samplerate']
-
+    FREQ = sd.query_devices(None, 'input')['default_samplerate']
     # Set the duration of the recording
-    duration = 5 # seconds
+    DURATION = 5 # seconds
+    # Set the sample duration
+    CHUNK = int(FREQ * DURATION)
 
     print('Listening...')
     while not stop_event.is_set():
@@ -17,12 +18,12 @@ def listen(stop_event, recordings_path):
 
         # Start recorder with the given values of duration and sample frequency
         # PTL Note: I had to change the channels value in the original code to fix a bug
-        recording = sd.rec(int(freq * duration), samplerate=freq, channels=2)
+        recording = sd.rec(CHUNK, samplerate=FREQ, channels=2)
 
         # Record audio for the given number of seconds
         sd.wait()
 
         # Convert the NumPy array to audio file
-        wv.write(os.path.join(recordings_path, f"{filename}.wav"), recording, freq, sampwidth=2)
+        wv.write(os.path.join(recordings_path, f"{filename}.wav"), recording, FREQ, sampwidth=2)
 
     print('Stopped listening')
