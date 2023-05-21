683b4b9a09d51da773bb623f9168b97f4bd5a1c7
AuracleTech <virgilfabre@outlook.com>
2023-05-21 14:35:08 -0700

    Fix silence threshold

--- recorder.py
+++ recorder.py
@@ -13,16 +13,17 @@ def listen(stop_event, recordings_path):
     # OpenAI whisper uses 16kHz sample rate by default
     RATE = 16000
     # Set the sample duration
-    CHUNK = 160
-
+    FRAME_LENGTH = RATE // 10
+    # Proportion of the frame length to be considered as silence
+    CHUNK_SILENT_THRESHOLD = 0.5
+    # Silence RMS ceiling
     SILENCE_RMS_CEILING = 64
-    SILENCE_MIN_FRAMES = CHUNK / 4
 
     # Initialize PyAudio
     audio = pyaudio.PyAudio()
 
     # Open microphone stream
-    stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
+    stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=FRAME_LENGTH)
 
     print('Listening...')
     while not stop_event.is_set():
@@ -31,12 +32,12 @@ def listen(stop_event, recordings_path):
 
         while True:
             # Read audio data from the stream
-            data = stream.read(CHUNK)
+            data = stream.read(FRAME_LENGTH)
             frames.append(data)
 
             # Check for silence
             chunk = np.frombuffer(data, dtype=np.int16)
-            rms_features = librosa.feature.rms(y=chunk, frame_length=CHUNK, hop_length=CHUNK)
+            rms_features = librosa.feature.rms(y=chunk, frame_length=FRAME_LENGTH, hop_length=FRAME_LENGTH)
             rms = np.average(rms_features)
 
             # If the audio is silent, increment the silent frame count
@@ -46,7 +47,7 @@ def listen(stop_event, recordings_path):
                 silent_frames = 0
             
             # If there are consecutive silent frames, stop recording
-            if silent_frames >= SILENCE_MIN_FRAMES:
+            if silent_frames >= CHUNK_SILENT_THRESHOLD:
                 break
         
         # If all frames are silent, skip saving the file
